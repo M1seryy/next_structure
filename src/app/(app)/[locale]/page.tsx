ad04a7adff1f6a44f589ed152533a299f6736166
M1seryy <nagibatartar@gmail.com>
2025-10-01 20:02:58 +0300

    hot fix

--- src/app/(app)/[locale]/page.tsx
+++ src/app/(app)/[locale]/page.tsx
@@ -2,7 +2,7 @@ import { type FC } from 'react'
 import { HydrationBoundary, dehydrate } from '@tanstack/react-query'
 import { getQueryClient } from '@/pkg/libraries/rest-api/service'
 import { HomeModule } from '@/app/modules/home'
-import { fetchPopularBooks } from '@/app/entities'
+import { fetchPopularBooks } from '@/app/entities/api'
 
 // interface
 interface IProps {
